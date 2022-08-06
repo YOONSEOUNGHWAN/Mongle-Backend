f3198f302681e38def9ad4e157ac99459ba18ad6
Seungyong Choi <ppyooy336@naver.com>
2022-08-06 13:27:46 +0900

    Feat: Add s3 uploadFile function

--- src/main/java/com/rtsj/return_to_soju/ReturnToSojuApplication.java
+++ src/main/java/com/rtsj/return_to_soju/ReturnToSojuApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReturnToSojuApplication {
+
+
+//	static {
+//		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReturnToSojuApplication.class, args);
 	}
