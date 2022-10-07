a10da8b473be82c09f5652fc1cbd7ed9fddb98a7
Seungyong Choi <ppyooy336@naver.com>
2022-10-07 15:04:16 +0900

    Feat: Add getMonthStatistics (0.3) Have to do

--- src/main/java/com/rtsj/return_to_soju/repository/CalenderRepositoryImpl.java
+++ src/main/java/com/rtsj/return_to_soju/repository/CalenderRepositoryImpl.java
@@ -34,9 +34,10 @@ public class CalenderRepositoryImpl implements CalenderRepositoryCustom {
                         calender.sad
                 )).from(calender)
                 .orderBy(calender.date.asc())
-                .join(user).on(user.id.eq(userId))
-                .where(calender.date.between(startDate,endDate))
+                .where(calender.date.between(startDate, endDate), calender.user.id.eq(userId))
                 .fetch();
+
+
     }
 
 }
